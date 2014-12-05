c110ae88fc2db7a32a85a075c7171b50aa71271e
Suhan Mao <smao@dataini.com>
2014-12-05 18:34:41 +0800

    fix

--- pynlpini/app/essay2tag/web_api.py
+++ pynlpini/app/essay2tag/web_api.py
@@ -17,6 +17,8 @@ base_dir = os.path.dirname(__file__)
 
 phrase2vector = Word2Vector.get_phrase_model()
 vocabs = phrase2vector.vocab.keys()
+word2vector = Word2Vector.get_word_model()
+
 with open(base_dir + "/tag.csv") as tag_file:
     tags = [line.decode("utf-8").strip() for line in tag_file.readlines()]
 
@@ -33,6 +35,21 @@ def tag():
         text = request.args.get('txt')
     words = extract_tags(text, 100) | where(lambda x: x in vocabs) | as_list
     tags_result = tags | select(lambda tag: [tag, phrase2vector.n_similarity(words, [tag])]) | sort(
+        key=lambda x: x[1], reverse=True) | as_list
+    return json.dumps(tags_result, ensure_ascii=False)
+
+@app.route('/tag2', methods=['GET', 'POST'])
+def tag():
+    global word2vector
+    if request.args.get('txt') is None:
+        if request.form.get('txt') is None:
+            abort(400)
+        else:
+            text = request.form.get('txt')
+    else:
+        text = request.args.get('txt')
+    words = extract_tags(text, 100) | where(lambda x: x in vocabs) | as_list
+    tags_result = tags | select(lambda tag: [tag, word2vector.n_similarity(words, [tag])]) | sort(
         key=lambda x: x[1], reverse=True) | as_list
     return json.dumps(tags_result, ensure_ascii=False)
 
